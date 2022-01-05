28c8f7a493313c4cf25b098ed445a4764c9b0a82
AJ-Kulundu <jameskulundu@gmail.com>
2022-01-05 16:19:33 +0300

    Creating network artifacts using configtxgen command

---
+++ artifacts/channel/create-artifacts.sh
@@ -0,0 +1,32 @@
+# System channel
+SYS_CHANNEL="sys-channel"
+
+# channel name defaults to "mychannel"
+CHANNEL_NAME="mychannel"
+
+echo $CHANNEL_NAME
+
+# Generate System Genesis block
+#configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
+
+
+# Generate channel configuration block
+#configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
+
+#echo "#######    Generating anchor peer update for AdminMSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./AdminMSPanchors.tx -channelID $CHANNEL_NAME -asOrg AdminMSP
+
+#echo "#######    Generating anchor peer update for InspectorMSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./InspectorMSPanchors.tx -channelID $CHANNEL_NAME -asOrg InspectorMSP
+
+#echo "#######    Generating anchor peer update for HarvesterMSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./HarvesterMSPanchors.tx -channelID $CHANNEL_NAME -asOrg HarvesterMSP
+
+#echo "#######    Generating anchor peer update for ExpoerterMSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./ExporterMSPanchors.tx -channelID $CHANNEL_NAME -asOrg ExpoerterMSP
+
+#echo "#######    Generating anchor peer update for ImporterMSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./ImporterMSPanchors.tx -channelID $CHANNEL_NAME -asOrg ImporterMSP
+
+#echo "#######    Generating anchor peer update for ProcessorMSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./ProcessorMSPanchors.tx -channelID $CHANNEL_NAME -asOrg ProcessorMSP
