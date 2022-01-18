f5f6f2271f64aa920f3ef04437218800dd6891b0
AJ-Kulundu <jameskulundu@gmail.com>
2022-01-18 19:29:13 +0300

    Full script for artifact creation

--- artifacts/channel/create-artifacts.sh
+++ artifacts/channel/create-artifacts.sh
@@ -1,3 +1,12 @@
+chmod -R 0755 ./crypto-config
+# Delete existing artifacts
+rm -rf ./crypto-config
+rm genesis.block mychannel.tx
+rm -rf ../../channel-artifacts/*
+
+#Generate Crypto artifactes for organizations
+cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
+
 # System channel
 SYS_CHANNEL="sys-channel"
 
@@ -7,11 +16,11 @@ CHANNEL_NAME="mychannel"
 echo $CHANNEL_NAME
 
 # Generate System Genesis block
-#configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
+configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
 
 
 # Generate channel configuration block
-#configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
+configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
 
 #echo "#######    Generating anchor peer update for AdminMSP  ##########"
 configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./AdminMSPanchors.tx -channelID $CHANNEL_NAME -asOrg AdminMSP
@@ -23,7 +32,7 @@ configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Inspe
 configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./HarvesterMSPanchors.tx -channelID $CHANNEL_NAME -asOrg HarvesterMSP
 
 #echo "#######    Generating anchor peer update for ExpoerterMSP  ##########"
-configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./ExporterMSPanchors.tx -channelID $CHANNEL_NAME -asOrg ExpoerterMSP
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./ExporterMSPanchors.tx -channelID $CHANNEL_NAME -asOrg ExporterMSP
 
 #echo "#######    Generating anchor peer update for ImporterMSP  ##########"
 configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./ImporterMSPanchors.tx -channelID $CHANNEL_NAME -asOrg ImporterMSP
